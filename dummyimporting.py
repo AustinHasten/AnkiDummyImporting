ac3f3b1f5ea7b4d2b28f4f5155a7d1996965ebb0
Austin Hasten <austinhasten@live.com>
2018-03-06 11:26:26 -0600

    Allow dev to set delimiter, importMode, and allowHTML.

--- dummyimporting.py
+++ dummyimporting.py
@@ -5,22 +5,23 @@ from aqt.importing import ImportDialog
 from anki.importing.csvfile import TextImporter
 
 class DummyImporter(TextImporter):
-    def __init__(self, flds, data, col):
+    def __init__(self, col, flds, delimiter, data):
         super().__init__(col, None)
         self.flds = flds
         self.data = data
+        self.delimiter = delimiter
         self.numFields = len(self.flds)
-        self.delimiter = ';'
-        self.dialect = None
         self.fileobj = open('dummy', 'w+')
 
 class ImportDialog(ImportDialog):
-    def __init__(self, mw, flds, data):
-        super().__init__(mw, DummyImporter(flds, data, mw.col))
+    def __init__(self, mw, flds, data, delimiter=';', importMode=2, allowHTML=True):
+        self.importMode = importMode
+        self.allowHTML = allowHTML
+        super().__init__(mw, DummyImporter(mw.col, flds, delimiter, data))
 
     def exec_(self):
-        self.frm.importMode.setCurrentIndex(2)
-        self.frm.importMode.close()
+        self.frm.importMode.setCurrentIndex(self.importMode)
+        self.frm.allowHTML.setCheckState(self.allowHTML)
         self.frm.allowHTML.close()
         self.frm.autoDetect.close()
         super().exec_()
